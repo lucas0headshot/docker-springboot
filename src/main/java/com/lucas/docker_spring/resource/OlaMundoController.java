18d0ac9a0f02a7388cb36fe472410ad0752ee7f4
lucas0headshot <lucas0headshot@gmail.com>
2024-10-25 20:22:28 -0300

    refactor: add @GetMapping em OlaMundoController

--- src/main/java/com/lucas/docker_spring/resource/OlaMundoController.java
+++ src/main/java/com/lucas/docker_spring/resource/OlaMundoController.java
@@ -1,12 +1,14 @@
 package com.lucas.docker_spring.resource;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ola-mundo")
 public class OlaMundoController {
+    @GetMapping
     public ResponseEntity<String> get() {
         return ResponseEntity.ok("Olá, Mundo!");
     }
